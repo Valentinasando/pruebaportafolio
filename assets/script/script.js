bcce0e0207d9ff2989f70ba24e13d13b86982d90
Valentina Sandoval <va.sadovala@gmail.com>
2020-12-28 15:43:57 -0300

    cambios carrusel

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,10 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  $('.carousel').carousel({
+    interval: 2000
+  })
+  $('#myCarousel').on('slide.bs.carousel', function () {
+    // do something...
+  })
